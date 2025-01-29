beb822740328f5ebaf9c291617d4dbde579d8b9f
jpal91 <pallanschja@gmail.com>
2025-01-29 12:57:39 -0500

    logging, package.json

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 import createApi from "./api";
 import ServiceRegistry from "./service-registry";
+import pino from "pino";
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || "info",
+  transport: {
+    target: "pino-pretty",
+  },
+});
 
 const port = process.env.PORT || 3002;
-const registry = new ServiceRegistry();
+const registry = new ServiceRegistry({ logger });
 const app = createApi(registry);
 
 const server = app.listen(port, () => {
-  console.log(`Service registry listening on port ${port}`);
+  logger.info(`Service registry listening on port ${port}`);
 });
 
 const shutdown = () => {
-  console.debug("Closing server");
+  logger.debug("Closing server");
   server.close(() => {
-    console.debug("Service Registry Closed");
+    logger.debug("Service Registry Closed");
   });
 };
 
